c7d10bf909823f7f815b81d1b84c7e0e3256a18b
Enoor <enoor.ikhiede@gmail.com>
2023-06-05 12:12:10 -0600

    Implementing findKey

---
+++ findKey.js
@@ -0,0 +1,34 @@
+// FUNCTION IMPLEMENTATION using literals
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual}  ===  ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  for (let key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+
+  return undefined;
+
+};
+
+const exampleObject = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+};
+
+const result = findKey(exampleObject, x => x.stars === 2);
+console.log(result);
+
+assertEqual(findKey(exampleObject, x => x.stars === 1), "Blue Hill");
+assertEqual(findKey(exampleObject, x => x.stars === 3), undefined);
