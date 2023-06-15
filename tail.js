0bf20fa879bd51cec7b0d50b8a44897c118d952c
Enoor <enoor.ikhiede@gmail.com>
2023-06-15 08:47:14 -0600

    Refactoring Tail with export function

--- tail.js
+++ tail.js
@@ -1,15 +1,16 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual}  ===  ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion Passed: ${actual}  ===  ${expected}`);
+//   } else {
+//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
+
+const assertEqual = require('./assertEqual');
+
 
 function tail(array) {
   return array.slice(1); 
 }
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); 
-assertEqual(words.length, 3); 
+module.exports = tail;
