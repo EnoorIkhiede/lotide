a0b9a931142a360c11a6e9944aa8e6ad9f415340
Enoor <enoor.ikhiede@gmail.com>
2023-06-15 14:25:18 -0600

    Implementing Mocha and Chai to test function

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,14 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); 
-assertEqual(words.length, 3); 
+// assertEqual(words.length, 3); 
+
+describe("#tail", () => {
+  it("returns 3 for words length", () => {
+    assert.strictEqual(words.length, 3);
+  });
+});
+
